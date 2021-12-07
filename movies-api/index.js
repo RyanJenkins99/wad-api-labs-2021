d3f8485e99fd6bb42544bdf940e47dc56d71f727
ryan <ryanjenkins1299@Gmail.com>
2021-12-07 23:21:16 +0000

    Mongo lab Error Handling

--- movies-api/index.js
+++ movies-api/index.js
@@ -21,4 +21,16 @@ app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
 
-app.use('/api/users', usersRouter); 
+const errHandler = (err, req, res, next) => {
+    /* if the error in development then send stack trace to display whole error,
+    if it's in production then just send error message  */
+    if(process.env.NODE_ENV === 'production') {
+      return res.status(500).send(`Something went wrong!`);
+    }
+    res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+  };
+  
+  
+  app.use('/api/users', usersRouter);
+  
+  app.use(errHandler); 
