0a8b4b2057722b435cdaccf315524246a306434d
ryan <ryanjenkins1299@Gmail.com>
2021-12-08 02:00:43 +0000

    Topic 10 - Passport

--- movies-api/index.js
+++ movies-api/index.js
@@ -6,6 +6,7 @@ import './seedData'
 import usersRouter from './api/users';
 import session from 'express-session';
 import authenticate from './authenticate';
+import passport from './authenticate';
 import genresRouter from './api/genres'
 
 dotenv.config();
@@ -16,14 +17,11 @@ const port = process.env.PORT;
 
 app.use(express.static('public'));
 
-app.use('/api/movies', authenticate, moviesRouter);
+// Add passport.authenticate(..)  to middleware stack for protected routes​
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter)
 app.use('/api/genres', genresRouter);
 
-app.use(session({
-    secret: 'ilikecake',
-    resave: true,
-    saveUninitialized: true
-  }));
+app.use(passport.initialize());
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
