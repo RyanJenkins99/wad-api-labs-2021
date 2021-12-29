e96056afb54e4a4361623e20da2f6210904af0ff
ryan <ryanjenkins1299@Gmail.com>
2021-12-29 18:31:34 +0000

    set up

--- movies-api/api/movies/users/index.js
+++ movies-api/api/movies/users/index.js
@@ -1,47 +1,45 @@
 import express from 'express';
 import User from './userModel';
 import asyncHandler from 'express-async-handler';
-
-const router = express.Router(); // eslint-disable-line
 import jwt from 'jsonwebtoken';
 import movieModel from '../movies/movieModel';
-import { set } from 'mongoose';
-
+const router = express.Router(); // eslint-disable-line
 // Get all users
 router.get('/', async (req, res) => {
     const users = await User.find();
     res.status(200).json(users);
 });
 
+// register
 router.post('/',asyncHandler( async (req, res, next) => {
+    var regularExpression=/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/;
     if (!req.body.username || !req.body.password) {
       res.status(401).json({success: false, msg: 'Please pass username and password.'});
     }
-
     if (req.query.action === 'register') {
-        if(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/.test(req.body.password)){
-            await User.create(req.body).catch(next);}
-        else{
-            res.status(401).json({code: 401,msg: 'Authentication failed. Password to weak'});
-        }
-        res.status(201).json({code: 201, msg: 'Successful created new user.'     });
-      } else {
-        const user = await User.findByUserName(req.body.username).catch(next);
-          if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
-          user.comparePassword(req.body.password, (err, isMatch) => {
-            if (isMatch && !err) {
-              // if user is found and password matches, create a token
-              const token = jwt.sign(user.username, process.env.SECRET);
-              // return the information including token as JSON
-              res.status(200).json({success: true, token: 'BEARER ' + token});
-            } else {
-              res.status(401).json({code: 401,msg: 'Authentication failed. Wrong password.'});
-            }
-          });
-        }
-    }));
     
-router.put('/:id', async (req, res) => {
+        if(regularExpression.test(req.body.password)){
+            await User.create(req.body).catch(next);
+            res.status(201).json({code: 201, msg: 'Successful created new user.'  });   
+        }
+
+    } else {
+      const user = await User.findByUserName(req.body.username).catch(next);
+        if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
+        user.comparePassword(req.body.password, (err, isMatch) => {
+          if (isMatch && !err) {
+            // if user is found and password matches, create a token
+            const token = jwt.sign(user.username, process.env.SECRET);
+            // return the information including token as JSON
+            res.status(200).json({success: true, token: 'BEARER ' + token});
+          } else {
+            res.status(401).json({code: 401,msg: 'Authentication failed. Wrong password.'});
+          }
+        });
+      }
+  }));
+ // Update a user
+ router.put('/:id', async (req, res) => {
     if (req.body._id) delete req.body._id;
     const result = await User.updateOne({
         _id: req.params.id,
@@ -52,22 +50,29 @@ router.put('/:id', async (req, res) => {
         res.status(404).json({ code: 404, msg: 'Unable to Update User' });
     }
 });
+function hasDuplicates(array) {
+    var valuesSoFar = Object.create(null);
+    for (var i = 0; i < array.length; ++i) {
+        var value = array[i];
+        if (value in valuesSoFar) {
+            return true;
+        }
+        valuesSoFar[value] = true;
+    }
+    return false;
+}
 
-//Add a favourite. No Error Handling Yet. Can add duplicates too!
 router.post('/:userName/favourites', asyncHandler(async (req, res) => {
+
     const newFavourite = req.body.id;
     const userName = req.params.userName;
     const movie = await movieModel.findByMovieDBId(newFavourite);
     const user = await User.findByUserName(userName);
-    //tempFav = awaituser.favourites
-    //console.log(tempFav)
-    //await user.favourites=Set(tempFav)
-
-    if(await user.favourites.includes(movie.id)){
-        await user.favourites.push(movie._id);
-    }
+    if(!hasDuplicates(user)){
+    await user.favourites.push(movie._id);
     await user.save(); 
     res.status(201).json(user); 
+    }
   }));
 
   router.get('/:userName/favourites', asyncHandler( async (req, res) => {
@@ -75,5 +80,4 @@ router.post('/:userName/favourites', asyncHandler(async (req, res) => {
     const user = await User.findByUserName(userName).populate('favourites');
     res.status(201).json(user.favourites);
   }));
-
-export default router; 
+export default router;
