e9b99bf3c4914827ace3fe67ecea8f9440bb4b1f
ryan <ryanjenkins1299@Gmail.com>
2021-12-29 19:57:16 +0000

    I've completed all my labs

--- movies-api/api/movies/movies/index.js
+++ movies-api/api/movies/movies/index.js
@@ -3,6 +3,9 @@ import { movies, movieReviews, movieDetails } from './moviesData';
 import uniqid from 'uniqid';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
+import {
+    getUpcomingMovies
+  } from '../tmdb-api';
 
 const router = express.Router(); 
 router.get('/', asyncHandler(async (req, res) => {
@@ -19,6 +22,11 @@ router.get('/', asyncHandler(async (req, res) => {
 
     res.status(200).json(returnObject);
 }));
+
+router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+  }));
 
 // Get movie details
 router.get('/:id', asyncHandler(async (req, res) => {
